31bd1b87ee31733103ba2065b2fe33a91a56f7a0
g.palyukh <g.palyukh@gmail.com>
2019-03-25 19:38:15 +0200

    added adaptive header

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".change-color").click(function () {
+        $("body").toggleClass("active");
+    });
+
+    $(".navbar-toggle").click(function () {
+        $(".navbar-toggle").toggleClass("open");
+    });
+
+});
+
